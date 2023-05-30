d0a7f36f2439db769891d155f804b8f0f10728a2
Petar Kresoja <petarkresoja5@gmail.com>
2023-05-31 00:28:40 +0200

    Add more details about the server

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -9,6 +9,15 @@
                 <td>Java 1.19.4</td>
             </tr>
             <tr>
+                <td>No cheating, No griefing, No stealing</td>
+            </tr>
+            <tr>
+                <td>ONLY paid clients (no crack)</td>
+            </tr>
+            <tr>
+                <td>ONLY java edition (no crossplay)</td>
+            </tr>
+            <tr>
                 <td>Chunk claims (/chunk)</td>
             </tr>
             <tr>
@@ -30,7 +39,7 @@
                 <td>Bats drop membranes</td>
             </tr>
             <tr>
-                <td>Hosted 24/7 in London on Intel Xeon 1220Lv2</td>
+                <td>Hosted 24/7 in London on <a href="https://www.intel.com/content/www/us/en/products/sku/65735/intel-xeon-processor-e31220l-v2-3m-cache-2-30-ghz/specifications.html">Intel Xeon 1220Lv2</a></td>
             </tr>
             <tr>
                 <td>Discord based authentication and webhooks</td>
@@ -39,3 +48,5 @@
         <p><router-link to="/join">Join our Discord server</router-link></p>
     </div>
 </template>
+<script setup lang="ts">
+</script>
