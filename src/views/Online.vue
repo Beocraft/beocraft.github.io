e4800ab9f646e94285b90cca5d799a5b3bfad52e
Petar Kresoja <petarkresoja5@gmail.com>
2025-08-24 19:56:31 +0200

    Add namemc server banner

--- src/views/Online.vue
+++ src/views/Online.vue
@@ -1,4 +1,8 @@
 <template>
+    <div class="namemc">
+        <iframe style="width:728px;height:90px;max-width:100%;border:none;display:block;margin:auto"
+            src="https://namemc.com/server/play.beocraft.net/embed" width="728" height="90"></iframe>
+    </div>
     <div v-if="error">
         <h3>Online Players</h3>
         <ErrorDisplay :message="error" keyword="online players" />
